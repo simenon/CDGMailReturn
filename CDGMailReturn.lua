a6d57f56c55e41a49a8374b3fe63bafdf94a02ff
simenon <albert@simenon.nl>
2014-04-07 20:39:34 +0200

    blanco

--- CDGMailReturn.lua
+++ CDGMailReturn.lua
@@ -5,15 +5,14 @@ local function CDGMailReturnUpdate()
 	for mailItem = 1, numMailItems, 1 do
 		nextMailId = GetNextMailId( nextMailId )
 		local senderDisplayName, senderCharacterName, subject, icon, unread, fromSystem, fromCustomerService, returned, numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived = GetMailItemInfo( nextMailId )
-		if numAttachments > 0 then
---			if string.upper( string.sub( subject,1,6 ) ) == ("RETURN" or "BOUNCE") then
---				d( string.format( "Item %d from %s with subject %s received ", nextMailId, senderDisplayName, subject ) )
-				d( string.format( "DBG senderDisplayName [%s] senderCharacterName [%s] subject [%s] icon [%s] unread [%s] fromSystem [%s] fromCustomerService [%s] returned [%s] numAttachments [%d] attachedMoney [%d] codAmount [%d] expiresInDays [%d] secsSinceReceived [%d]",  senderDisplayName, senderCharacterName, subject, icon, tostring( unread ), tostring( fromSystem ), tostring( fromCustomerService ), tostring( returned ), numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived ) )
---			end
+		if numAttachments > 0 and not fromSystem and not fromCustomerService and unread then
+			if string.upper( string.sub( subject,1,6 ) ) == ("RETURN" or "BOUNCE") then
+				ReturnMail( nextMailId )
+				d( string.format( "Item %d from %s with subject %s returned ", nextMailId, senderDisplayName, subject ) )
+			end
 		end
 	end
 end
-
 
 function CDGMailReturnUpdate_OnInitialized()
 	EVENT_MANAGER:RegisterForEvent("CDGMailReturn",EVENT_MAIL_INBOX_UPDATE, CDGMailReturnUpdate)
