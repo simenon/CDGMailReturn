64614e2bba2b09112c4a1c60d742113bef7a7c14
simenon <albert@simenon.nl>
2014-04-07 19:46:51 +0200

    Log to chat log

--- CDGMailReturn.lua
+++ CDGMailReturn.lua
@@ -2,7 +2,7 @@
 local function CDGMailReturnUpdate()
 	for MailItem = 1, GetNumMailItems() do
 		senderDisplayName, senderCharacterName, subject, icon, unread, fromSystem, fromCustomerService, returned, numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived = GetMailItemInfo(MailItem)
-		CDGMailReturnCounter:SetText(string.format("Item %d Sender %s Subject %s", MailItem, senderCharacterName, subject ))
+		d(string.format("Item %d Sender %s Subject %s", MailItem, senderCharacterName, subject ))
 	end
 end
 
