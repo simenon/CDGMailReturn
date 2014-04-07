39c6c51645e41b6451e54bce6d2ffbe9395ef4ee
simenon <albert@simenon.nl>
2014-04-07 20:31:17 +0200

    blanco

--- CDGMailReturn.lua
+++ CDGMailReturn.lua
@@ -9,7 +9,7 @@ local function CDGMailReturnUpdate()
 --			if string.upper( string.sub( subject,1,6 ) ) == ("RETURN" or "BOUNCE") then
 --				d( string.format( "Item %d from %s with subject %s received ", nextMailId, senderDisplayName, subject ) )
 				d( string.format( "DBG senderDisplayName [%s] senderCharacterName [%s] subject [%s] icon [%s] unread [%s] fromSystem [%s] fromCustomerService [%s] returned [%s] numAttachments [%d] attachedMoney [%d] codAmount [%d] expiresInDays [%d] secsSinceReceived [%d]",  senderDisplayName, senderCharacterName, subject, icon, unread, fromSystem, fromCustomerService, returned, numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived ) )
-			end
+--			end
 		end
 	end
 end
