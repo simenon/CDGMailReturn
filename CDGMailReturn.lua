99fbbc7b73ee2c79630ff7f8f17d8b04a5ce7e30
simenon <albert@simenon.nl>
2014-04-07 19:33:41 +0200

    Typo fix

--- CDGMailReturn.lua
+++ CDGMailReturn.lua
@@ -1,10 +1,11 @@
-function CDGMailReturnUpdate_OnInitialized()
-	EVENT_MANAGER:RegisterForEvent("CDGMailReturn",EVENT_MAIL_INBOX_UPDATE, CDGMailReturnUpdate)
-end
 
 local function CDGMailReturnUpdate()
 	for MailItem = 1, GetNumMailItems() do
 		senderDisplayName, senderCharacterName, subject, icon, unread, fromSystem, fromCustomerService, returned, numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived = GetMailItemInfo(MailItem)
 		CDGMailReturnCounter:SetText(string.format("Item %d Sender %s Subject %s", MailItem, senderCharacterName, subject ))
 	end
+end
+
+function CDGMailReturnUpdate_OnInitialized()
+	EVENT_MANAGER:RegisterForEvent("CDGMailReturn",EVENT_MAIL_INBOX_UPDATE, CDGMailReturnUpdate)
 end
