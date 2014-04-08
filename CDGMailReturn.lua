3d3ca25cf7502e8ba39f9248dceb0e03cb2703ba
simenon <albert@simenon.nl>
2014-04-08 18:19:58 +0200

    fixes

--- CDGMailReturn.lua
+++ CDGMailReturn.lua
@@ -4,9 +4,10 @@ local function CDGMailReturnUpdate()
 
 	for mailItem = 1, numMailItems, 1 do
 		nextMailId = GetNextMailId( nextMailId )
-		local senderDisplayName, senderCharacterName, subject, icon, unread, fromSystem, fromCustomerService, returned, numAttachments, attachedMoney, codAmount, expiresInDays, secsSinceReceived = GetMailItemInfo( nextMailId )
+		local senderDisplayName, _, subject, _, unread, fromSystem, fromCustomerService, _, numAttachments, _, _, _, _ = GetMailItemInfo( nextMailId )
 		if numAttachments > 0 and not fromSystem and not fromCustomerService and unread then
-			if string.upper( string.sub( subject,1,6 ) ) == ("RETURN" or "BOUNCE") then
+			if string.upper( string.sub( subject,1,6 ) ) == "RETURN" or 
+			   string.upper( string.sub( subject,1,6 ) ) == "BOUNCE" then
 				ReturnMail( nextMailId )
 				d( string.format( "Item %d from %s with subject %s returned ", nextMailId, senderDisplayName, subject ) )
 			end
